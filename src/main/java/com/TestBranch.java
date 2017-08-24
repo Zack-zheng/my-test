d713e8221d117c519369df7463d5cd642415c1ed
zack-zheng <121322680@qq.com>
2017-08-24 23:37:54 +0800

    test2 commit 1

---
+++ src/main/java/com/TestBranch.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by jack_ on 2017/8/24.
+ */
+public class TestBranch {
+    public static void main(String[] args) {
+        System.out.println("test2 commit 1");
+    }
+}
