bf40fcb95b7b1d7d74812556f7e8601aec6dc54e
zhengzz <zhengzz@yanxintec.com>
2017-08-25 15:13:46 +0800

    test branch 2 commit 4 after merge to master

--- src/main/java/com/TestBranch.java
+++ src/main/java/com/TestBranch.java
@@ -6,5 +6,10 @@ package com;
 public class TestBranch {
     public static void main(String[] args) {
         System.out.println("test branch 2 commit 3");
+        printInfo();
+    }
+
+    private static void printInfo() {
+        System.out.println("test branch 2 commit 4 after merge to master");
     }
 }
