67ff27d8d4fb413934b994cc65d29dea5f4d6c9e
zhengzz <zhengzz@yanxintec.com>
2017-08-25 14:53:01 +0800

    test branch 2 commit 3

--- src/main/java/com/TestBranch.java
+++ src/main/java/com/TestBranch.java
@@ -5,6 +5,6 @@ package com;
  */
 public class TestBranch {
     public static void main(String[] args) {
-        System.out.println("test branch 2 commit 2");
+        System.out.println("test branch 2 commit 3");
     }
 }
