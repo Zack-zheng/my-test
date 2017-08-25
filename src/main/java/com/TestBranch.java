d0a955d9ee80e2aa108fea974a79fbf3b1709e35
zhengzz <zhengzz@yanxintec.com>
2017-08-25 14:49:35 +0800

    test branch 1 commit 1

---
+++ src/main/java/com/TestBranch.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by zhengzz on 2017/8/25.
+ */
+public class TestBranch {
+    public static void main(String[] args) {
+        System.out.println("test branch 1 commit 1");
+    }
+}
